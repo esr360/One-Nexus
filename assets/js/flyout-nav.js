6e1e5b8ab55aaaa75e11584fd56c222ebe1992a4
Ed Reed <esr360@live.com>
2015-03-28 18:59:54 +0000

    replacing third-party off-canvas nav with custom

---
+++ assets/js/flyout-nav.js
@@ -0,0 +1,54 @@
+
+/* Flyout Navigation
+================================================================ */
+
+$(document).ready(function() {
+
+    var fnContainer = $('#flyout');
+
+    // create the flyout nav HTML
+    function flyoutNav() {
+
+        $("#flyout-trigger").detach().prependTo('body') // relocate the flyout-trigger in the DOM
+        $("#app-nav > ul").clone().appendTo(fnContainer); // clone the main nav into the flyout nav container
+
+    } // End flyoutNav()
+
+    $(flyoutNav);
+
+}); // end document.ready
+
+$(window).load(function(){
+
+    function toggleFlyout(state) {
+
+        function flyoutEvents(){
+            $('body').toggleClass('flyout-active');
+            $('#flyout-trigger').toggleClass('nav-trigger-animate');
+            $('#site-overlay').toggleClass('visible');
+        }
+
+        if($('body').hasClass('flyout-active')) {  // is the flyout nav currently toggled?
+            // yes we are
+            if(state != 1) {
+                flyoutEvents();
+            }
+        } else {
+            // no, we are not
+            if(state != 0) {
+                flyoutEvents();
+            }
+        }
+
+    } // end toggleFlyout()
+
+    // toggle the flyout nav
+    $('#flyout-trigger').click(function() {
+        toggleFlyout();
+    });
+
+    $('#flyout-nav a, .site-overlay').click(function() {
+        toggleFlyout(0);
+    });
+
+}); // end window.load
