e48f8acbf25a3a822e16e9e9ca9e021758786d22
Ed Reed <esr360@live.com>
2015-04-26 21:57:48 +0100

    deleted/renamed files

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-//@prepros-append accordion.js
-//@prepros-append carousel.js
-//@prepros-append modal.js
-//@prepros-append off-canvas-nav.js
-//@prepros-append tabs.js
-//@prepros-append tooltip.js
-
-//@prepros-append global.js
