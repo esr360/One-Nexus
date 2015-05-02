4a1e9aa4503f0c581d4927be2122ccdaa4c46f95
Ed Reed <esr360@live.com>
2015-05-02 16:22:36 +0100

    completing adaptive-columns js

--- assets/js/adaptive-columns.js
+++ assets/js/adaptive-columns.js
@@ -108,6 +108,174 @@ fiveCols.owlCarousel({
 });
 
 //-----------------------------------------------------------------
+// 6 Columns
+//-----------------------------------------------------------------
+
+var fiveCols = $('.6-cols');
+
+fiveCols.owlCarousel({
+    items: 1,
+    loop: false,
+    dots: false,
+    nav: true,
+    navText: [],
+    margin: 31,
+    slideBy: 1,
+    mouseDrag: false,
+    responsive:{
+        460 :{                
+            items: 2
+        },
+        720 :{                
+            items: 4
+        },
+        940 :{                
+            items: 6
+        }
+    }
+});
+
+//-----------------------------------------------------------------
+// 7 Columns
+//-----------------------------------------------------------------
+
+var fiveCols = $('.7-cols');
+
+fiveCols.owlCarousel({
+    items: 1,
+    loop: false,
+    dots: false,
+    nav: true,
+    navText: [],
+    margin: 31,
+    slideBy: 1,
+    mouseDrag: false,
+    responsive:{
+        460 :{                
+            items: 3
+        },
+        720 :{                
+            items: 5
+        },
+        940 :{                
+            items: 7
+        }
+    }
+});
+
+//-----------------------------------------------------------------
+// 8 Columns
+//-----------------------------------------------------------------
+
+var fiveCols = $('.8-cols');
+
+fiveCols.owlCarousel({
+    items: 1,
+    loop: false,
+    dots: false,
+    nav: true,
+    navText: [],
+    margin: 31,
+    slideBy: 1,
+    mouseDrag: false,
+    responsive:{
+        460 :{                
+            items: 3
+        },
+        720 :{                
+            items: 6
+        },
+        940 :{                
+            items: 8
+        }
+    }
+});
+
+//-----------------------------------------------------------------
+// 9 Columns
+//-----------------------------------------------------------------
+
+var fiveCols = $('.9-cols');
+
+fiveCols.owlCarousel({
+    items: 1,
+    loop: false,
+    dots: false,
+    nav: true,
+    navText: [],
+    margin: 31,
+    slideBy: 1,
+    mouseDrag: false,
+    responsive:{
+        460 :{                
+            items: 3
+        },
+        720 :{                
+            items: 6
+        },
+        940 :{                
+            items: 9
+        }
+    }
+});
+
+//-----------------------------------------------------------------
+// 10 Columns
+//-----------------------------------------------------------------
+
+var fiveCols = $('.10-cols');
+
+fiveCols.owlCarousel({
+    items: 1,
+    loop: false,
+    dots: false,
+    nav: true,
+    navText: [],
+    margin: 31,
+    slideBy: 1,
+    mouseDrag: false,
+    responsive:{
+        460 :{                
+            items: 3
+        },
+        720 :{                
+            items: 6
+        },
+        940 :{                
+            items: 10
+        }
+    }
+});
+
+//-----------------------------------------------------------------
+// 11 Columns
+//-----------------------------------------------------------------
+
+var fiveCols = $('.11-cols');
+
+fiveCols.owlCarousel({
+    items: 1,
+    loop: false,
+    dots: false,
+    nav: true,
+    navText: [],
+    margin: 31,
+    slideBy: 1,
+    mouseDrag: false,
+    responsive:{
+        460 :{                
+            items: 3
+        },
+        720 :{                
+            items: 6
+        },
+        940 :{                
+            items: 11
+        }
+    }
+});
+
+//-----------------------------------------------------------------
 // 12 Columns
 //-----------------------------------------------------------------
 
