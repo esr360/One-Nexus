770b0d77ea9e571ad83631246cdb2f02be41f6ae
Edmund Reed <Edmund Reed>
2016-11-24 01:01:14 +0000

    updating modules

--- assets/modules/elements/carousels/carousels.js
+++ assets/modules/elements/carousels/carousels.js
@@ -11,7 +11,7 @@
      * @example
      *     $('.carousel').carousel({
      *         pagination: false,
-     *         options: {
+     *         owl: {
      *             items: 1 
      *         }
      *     });
