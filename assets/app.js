52111636d5f2712186c8669837ed2e9101080753
Edmund <edmund@SOAR-298.local>
2017-06-08 11:13:24 +1000

    updating packages

--- assets/app.js
+++ assets/app.js
@@ -7,14 +7,14 @@
 //*****************************************************************
 
 //import synergy from '../node_modules/Synergy/src/js/synergy';
-import synergy from '../../../Synergy/src/js/synergy';
+import synergy  from '../../../Synergy/src/js/synergy';
 import Flickity from 'flickity';
 
 // Elements
 //*****************************************************************
 
 import { accordion } from './modules/elements/accordions/accordions';
-import { carousel } from './modules/elements/carousels/carousels';
+import { carousel  } from './modules/elements/carousels/carousels';
 
 // Config
 //*****************************************************************
