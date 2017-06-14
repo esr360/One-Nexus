fb963254d7235d4b65451afb27c3462ec5e09b04
esr360 <esr360@live.com>
2017-06-15 01:32:30 +1000

    starting to add carousel functionality

--- assets/modules/elements/carousels/carousels.js
+++ assets/modules/elements/carousels/carousels.js
@@ -8,10 +8,22 @@ import defaults from './carousels.json';
  * @param {(String|Object)} els
  * @param {Object} custom
  */
-export function carousel(els, custom) {
+export function carousel(els = 'carousel', custom) {
 
-    app.synergy(els, function(el, options) {
+    custom = app.custom('carousels', custom);
 
+    app.Synergy(els, function(el, options) {
+        // Create new Flickity instance
+        var carousel = new app.Flickity(el, options.Flickity);
+
+        // Add appropriate classes to carousel elements
+        for (let [key, value] of Object.entries(options.components)) {
+            if (value) {
+                const identifier = `${options.name}_${key}`;
+                const component = el.querySelector(value);
+                component.classList.add(identifier);
+            }
+        }
     }, defaults, custom);
 
     app.config.carousels = Object.assign(defaults.carousels, custom);
