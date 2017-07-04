a1ee2fa2bef918aa8c0a7cbb7ee0988264de28ab
esr360 <esr360@live.com>
2017-07-04 23:40:18 +1000

    updating breakpoint function

--- assets/tools/js/app.breakpoint.js
+++ assets/tools/js/app.breakpoint.js
@@ -1,7 +1,14 @@
-function breakpoint(media, value) {
+/**
+ * Breakpoint
+ * 
+ * @access public
+ * 
+ * @param {String} media
+ * @param {String} value
+ */
+export function breakpoint(media, value) {
     if (value.indexOf('break') == 0) {
-	    //return window.matchMedia('(' + media + ':' + _modules['grid']['breakpoints'][value] + ')').matches;
-    } else {
-        //return window.matchMedia('(' + media + ':' + value + ')').matches;
+	    return window.matchMedia(`(${media}: ${app.config.grid.breakpoints[value]})`).matches;
     }
+    return window.matchMedia('(' + media + ':' + value + ')').matches;
 }
