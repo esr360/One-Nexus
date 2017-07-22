5a27a9a6cc372501c077def17b307d40fe37ba40
esr360 <esr360@live.com>
2017-07-23 00:31:08 +1000

    adding dropdown functionality to sidenav

--- assets/modules/objects/side-nav/side-nav.js
+++ assets/modules/objects/side-nav/side-nav.js
@@ -15,27 +15,36 @@ export function sideNav(els = 'sideNav', custom) {
 
     app.Synergy(els, (el, options) => {
 
+        // toggle side nav on trigger click
         app.Synergy(options.name).component('trigger').forEach(trigger => {
             trigger.addEventListener('click', () => exports.toggle());
         });
 
+        // close side nav on component click
         app.Synergy(options.name).component('close').forEach(trigger => {
             trigger.addEventListener('click', () => exports.hide());
+        });
+
+        // insert dropdown toggle element where appropriate
+        el.querySelectorAll('li ul').forEach(dropdown => {
+            dropdown.parentNode.insertBefore(sideNavDropdownIcon(options), dropdown);
+            // set default collapse of dropdowns
+            if (!options.collapsible['open-by-default']) {
+                dropdown.classList.add('collapsed');
+            }
+        });
+
+        // toggle dropdown on element click
+        el.component('toggle').forEach(toggle => {
+            toggle.addEventListener('click', () => {
+                toggle.nextSibling.classList.toggle('collapsed')
+            });
         });
 
         exports.show = () => exports.toggle('show');
         exports.hide = () => exports.toggle('hide');
 
-        exports.toggle = operator => {
-            const state = (el.modifier('visible') || operator === 'hide') ? 'unset' : 'set';
-            const listener = (state === 'set') ? 'addEventListener' : 'removeEventListener';
-            // toggle sidenav
-            el.modifier('visible', state);
-            // toggle overlay
-            app.siteOverlay(options.overlay).toggle('overlaySideNav');
-            // toggle event handler to hide side-nav on overlay click
-            app.Synergy(options.overlay).query[0][listener]('click', exports.hide);
-        }
+        exports.toggle = operator => toggleSideNav(el, operator, options);
 
     }, defaults, custom);
 
@@ -43,3 +52,40 @@ export function sideNav(els = 'sideNav', custom) {
 
     return exports;
 };
+
+/**
+ * Show/Hide the Side Navigation
+ * 
+ * @access private
+ * 
+ * @param {HTMLElement} el - the side-nav HTML element 
+ * @param {('show'|'hide')} operator - the type of toggle to activate
+ * @param {Object} options - the module options to use
+ */
+function toggleSideNav(el, operator, options) {
+    // determine toggle state
+    const state = (el.modifier('visible') || operator === 'hide') ? 'unset' : 'set';
+    const listener = (state === 'set') ? 'addEventListener' : 'removeEventListener';
+
+    // toggle sidenav
+    el.modifier('visible', state);
+    // toggle overlay
+    app.siteOverlay(options.overlay).toggle('overlaySideNav');
+    // toggle event handler to hide side-nav on overlay click
+    app.Synergy(options.overlay).query[0][listener]('click', exports.hide);    
+}
+
+/**
+ * Create Dropdown Toggle Icon
+ * 
+ * @access private
+ * 
+ * @param {Object} options - the module options to use
+ */
+function sideNavDropdownIcon(options) {
+    const el = document.createElement('i');
+
+    el.classList.add(`${options.name}_toggle`, 'fa', options.collapsible.icon);
+
+    return el;  
+}
