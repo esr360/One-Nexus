41d10ee95495c1b94ce8db5b299c28e27e809020
esr360 <esr360@live.com>
2017-09-12 21:17:18 +1000

    adding 'app' as arg for media function

--- assets/tools/js/app.media.js
+++ assets/tools/js/app.media.js
@@ -6,7 +6,7 @@
  * @param {String} media
  * @param {String} value
  */
-export function media(media, value) {
+export function media(media, value, app) {
     if (value.indexOf('break') === 0) {
 	    return window.matchMedia(`(${media}: ${app.config.grid.breakpoints[value]})`).matches;
     }
