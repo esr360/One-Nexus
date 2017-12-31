3d6f5fe3c0ebaae32766a7f0f20862002cc860f8
esr360 <esr360@live.com>
2018-01-01 02:52:08 +1100

    attempting to compile with node-sass

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -16,7 +16,7 @@ import deepextend from 'deep-extend';
 import Browser from 'detect-browser';
 import Flickity from 'flickity';
 import PubSub from 'pubsub-js';
-import Synergy from 'Synergy';
+import Synergy from '../../../../Synergy/src/js/synergy';
 
 export { 
     Browser,
