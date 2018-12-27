6110f1eba993ad8614d5f437131ce4b87266eb17
esr360 <reede@ml029268.home>
2018-12-27 02:23:26 +0000

    updating navigation module

--- src/ui/modules/objects/navigation/assets/config.js
+++ src/ui/modules/objects/navigation/assets/config.js
@@ -2,43 +2,56 @@ export default theme => ({
     'name': 'navigation',
     'text-align': 'right',
     'font-size': '1em',
-    'dropdown-icon': '\\f107',
 
-    'dropdown': {
-        'min-width': '200px'
-    },
-
-    'item': {
-        'border': '1px solid transparent',
-
-        'modifier(top-level)': {
+    menu: {
+        item: {
             'gutter': '0.5em',
-            'padding': '0.5rem 0.75rem',
-            'background': 'transparent',
-            'transition': theme.core.transition,
-            'border-radius': '0',
+
+            link: {
+                'border': '1px solid transparent',
+                'transition': theme.core.transition,
+                'border-radius': '0',
+                'padding': '0.5rem 0.75rem',
+                'background': 'transparent',
+                'color': theme.colors.greyscale['grey-2']
+            },
 
             ':hover': {
-                'color': theme.colors.greyscale.white,
-                'background': 'transparent',
-                'border-color': theme.colors.greyscale.white
-            }
-        },
-
-        'modifier(depth)': {
-            'padding': '0.5rem 0.75rem',
-        },
-
-        'active': {
-            'color': theme.colors.greyscale.white
-        },
-
-        'has-child': {
-            'hover': {
-                'color': theme.colors.brand['brand-1'],
-                'background': theme.colors.greyscale['grey-1'],
-                'border': '1px solid transparent'
+                link: {
+                    'color': theme.colors.greyscale.white,
+                    'background': 'transparent',
+                    'border-color': theme.colors.greyscale.white
+                }
+            },
+    
+            'active': {
+                link: {
+                    'color': theme.colors.greyscale.white
+                }
+            },
+    
+            'modifier(has-child)': {
+                ':hover': {
+                    link: {
+                        'color': theme.colors.brand['brand-1'],
+                        'background': theme.colors.greyscale['grey-1'],
+                        'border': '1px solid transparent'
+                    }
+                }
             }
         }
-    }
+    },
+
+    dropdown: {
+        'min-width': '200px',
+        'transition': theme.core.transition,
+
+        item: {
+            link: {
+                'padding': '0.5rem 0.75rem',
+                'background': theme.colors.greyscale['grey-6'],
+                'color': theme.colors.greyscale.white
+            }
+        }
+    },
 })
